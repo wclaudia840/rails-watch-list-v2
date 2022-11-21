5c566990eda154e40db16a43d76c5d159ea21fd0
chenwang <wclaudia840@gmail.com>
2022-11-22 09:53:26 +1100

    rails-watch-list

--- Gemfile
+++ Gemfile
@@ -76,3 +76,5 @@ gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [:index, :create, :new, :show] do
+  resources :lists, only: [:index, :create, :new, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
