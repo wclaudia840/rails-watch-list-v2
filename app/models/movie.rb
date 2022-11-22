7ad847ae153e5aae7a8e00b1b0c3b85f0b32b01d
chenwang <wclaudia840@gmail.com>
2022-11-22 15:53:16 +1100

    add image uploading function

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-
+  has_one_attached :photo
   # validates :title, uniqueness: true, presence: true
   # validates :overview, presence: true
 end
